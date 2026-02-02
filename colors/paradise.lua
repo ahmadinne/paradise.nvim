dce0f4bf6156d82497f693f07afd1022b35f7347
ahmadinne <ahmadinne@proton.me>
2026-02-03 01:50:02 +0700

    feat: update

--- colors/paradise.lua
+++ colors/paradise.lua
@@ -1,1 +1,8 @@
-require("pd.cs")
+for handsome in pairs(package.loaded) do
+	if handsome:match(".*paradise.*") then
+		package.loaded[handsome] = nil
+	end
+end
+
+require 'paradise'.setup()
+require 'paradise'.colorscheme()
---
+++ lua/paradise/init.lua
@@ -0,0 +1,72 @@
+local M = {}
+
+---Change paradise option (vim.g.paradise_config.option)
+---It can't be changed directly by modifying that field due to a Neovim lua bug with global variables (paradise_config is a global variable)
+---@param opt string: option name
+---@param value any: new value
+function M.set_options(opt, value)
+    local cfg = vim.g.paradise_config
+    cfg[opt] = value
+    vim.g.paradise_config = cfg
+end
+
+---Apply the colorscheme (same as ':colorscheme paradise')
+function M.colorscheme()
+    vim.cmd("hi clear")
+    if vim.fn.exists("syntax_on") then vim.cmd("syntax reset") end
+    vim.o.termguicolors = true
+    vim.g.colors_name = "paradise"
+    require('paradise.highlights').setup()
+    require('paradise.terminal').setup()
+end
+
+local default_config = {
+    -- Main options --
+    transparent = false,     -- don't set background
+    term_colors = true,      -- if true enable the terminal
+    ending_tildes = false,    -- show the end-of-buffer tildes
+    cmp_itemkind_reverse = false,    -- reverse item kind highlights in cmp menu
+
+    -- Changing Formats --
+    code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
+    },
+
+    -- Lualine options --
+    lualine = {
+        transparent = false, -- center bar (c) transparency
+    },
+
+    -- Custom Highlights --
+    colors = {}, -- Override default colors
+    highlights = {}, -- Override highlight groups
+
+    -- Plugins Related --
+    diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true,   -- use undercurl for diagnostics
+        background = true,    -- use background color for virtual text
+    },
+}
+
+---Setup paradise.nvim options, without applying colorscheme
+---@param opts table: a table containing options
+function M.setup(opts)
+    if not vim.g.paradise_config or not vim.g.paradise_config.loaded then    -- if it's the first time setup() is called
+        vim.g.paradise_config = vim.tbl_deep_extend('keep', vim.g.paradise_config or {}, default_config)
+        M.set_options('loaded', true)
+    end
+    if opts then
+        vim.g.paradise_config = vim.tbl_deep_extend('force', vim.g.paradise_config, opts)
+    end
+end
+
+function M.load()
+  vim.api.nvim_command('colorscheme paradise')
+end
+
+return M
