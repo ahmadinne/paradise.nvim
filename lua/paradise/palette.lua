d2acc71afbfa2abdc8d0484e9d5d7dc112e25180
ahmadinne <ahmadinne@proton.me>
2026-02-03 01:53:37 +0700

    upd

--- lua/paradise/palette.lua
+++ lua/paradise/palette.lua
@@ -1,41 +1,43 @@
 return {
-	-- Core
-	black = "#151515",   -- base00
-	bg0 = "#151515",     -- base00
-	bg1 = "#1F1F1F",     -- base01
-	bg2 = "#2E2E2E",     -- base02
-	bg3 = "#424242",     -- base03
-	bg_d = "#151515",   -- base00
+	dark = {
+		-- Core
+		black = "#151515",   -- base00
+		bg0 = "#151515",     -- base00
+		bg1 = "#1F1F1F",     -- base01
+		bg2 = "#2E2E2E",     -- base02
+		bg3 = "#424242",     -- base03
+		bg_d = "#151515",   -- base00
 
-	-- Background accents
-	bg_blue = "#8DA3B9",   -- base0D
-	bg_yellow = "#D9BC8C", -- base0A
+		-- Background accents
+		bg_blue = "#8DA3B9",   -- base0D
+		bg_yellow = "#D9BC8C", -- base0A
 
-	-- Foreground
-	fg = "#E8E3E3",        -- base05
+		-- Foreground
+		fg = "#E8E3E3",        -- base05
 
-	-- Accents
-	purple = "#A988B0",    -- base0E
-	green = "#8C977D",     -- base0B
-	orange = "#D9BC8C",    -- base09
-	blue = "#8DA3B9",      -- base0D
-	yellow = "#D9BC8C",    -- base0A
-	cyan = "#8AA6A2",      -- base0C
-	red = "#B66467",       -- base08
+		-- Accents
+		purple = "#A988B0",    -- base0E
+		green = "#8C977D",     -- base0B
+		orange = "#D9BC8C",    -- base09
+		blue = "#8DA3B9",      -- base0D
+		yellow = "#D9BC8C",    -- base0A
+		cyan = "#8AA6A2",      -- base0C
+		red = "#B66467",       -- base08
 
-	-- Neutrals
-	grey = "#BBB6B6",       -- base04
-	light_grey = "#E8E3E3", -- base06
+		-- Neutrals
+		grey = "#BBB6B6",       -- base04
+		light_grey = "#E8E3E3", -- base06
 
-	-- Dark accents
-	dark_cyan = "#8AA6A2",   -- base0C
-	dark_red = "#B66467",    -- base08
-	dark_yellow = "#D9BC8C", -- base0A
-	dark_purple = "#A988B0", -- base0E
+		-- Dark accents
+		dark_cyan = "#8AA6A2",   -- base0C
+		dark_red = "#B66467",    -- base08
+		dark_yellow = "#D9BC8C", -- base0A
+		dark_purple = "#A988B0", -- base0E
 
-	-- Diff
-	diff_add = "#1F1F1F",    -- base01
-	diff_delete = "#2E2E2E", -- base02
-	diff_change = "#424242", -- base03
-	diff_text = "#8DA3B9",   -- base0D
+		-- Diff
+		diff_add = "#1F1F1F",    -- base01
+		diff_delete = "#2E2E2E", -- base02
+		diff_change = "#424242", -- base03
+		diff_text = "#8DA3B9",   -- base0D
+	}
 }
